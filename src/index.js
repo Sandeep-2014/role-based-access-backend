9bb07afde73f58642619252f1b4f81bfe078701c
Sandeep-2014 <sandeep23@navgurukul.org>
2025-04-09 14:41:19 +0530

    updated oringin url in cors middleware

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://127.0.0.1:5500", // Your frontend URL
+    origin: "https://role-based-access-frontend.onrender.com", // Your frontend URL
     // origin: process.env.CORS_ORIGIN,
     credentials: true, // Allow sending cookies
   }))
